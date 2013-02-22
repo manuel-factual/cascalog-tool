69afa627e1e63a0ddb87a14e17d67d3aec9b3d5d
Alex Robbins <alexander.j.robbins@gmail.com>
2013-02-22 10:36:15 -0800

    Fix bad project.clj

--- query-sandbox/project.clj
+++ query-sandbox/project.clj
@@ -3,10 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"factual" "http://maven.corp.factual.com/nexus/content/groups/public"
+                 "releases" "http://maven.corp.factual.com/nexus/content/repositories/releases"
+                 "snapshots" "http://maven.corp.factual.com/nexus/content/repositories/snapshots"}
+  :omit-default-repositories false
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.hadoop/hadoop-core "0.20.2-cdh3u4"]
                  [cascalog "1.9.0"]
-                 [cascalog-toolbox 1.0.1]
+                 [cascalog-toolbox "1.0.1"]
                  [cheshire "4.0.0"]
                  [org.slf4j/slf4j-api "1.7.2"]
                  [org.slf4j/slf4j-log4j12 "1.7.2"]]
