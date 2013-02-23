3ac1a88bc54281cad784937c4edca3729486625a
Alex Robbins <alexander.j.robbins@gmail.com>
2013-02-22 16:14:00 -0800

    Added persistence.

--- resources/public/javascript/index.js
+++ resources/public/javascript/index.js
@@ -9,12 +9,33 @@ function submitText(text) {
   setTimeout(pollAndUpdateRunnerOutput, 1000);
 }
 
+
 $(document).ready(function() {
-  var editor = ace.edit("editor");
+  editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/clojure");
+
+  function persistEditor() {
+    if(localStorage) {
+      localStorage["editor"] = editor.getValue();
+    }
+  }
+  function restoreEditor() {
+    if(localStorage) {
+        if (localStorage["editor"]) {
+            editor.setValue(localStorage["editor"]);
+            editor.clearSelection();
+        }
+    }
+  }
+  window.onbeforeunload = persistEditor;
+  restoreEditor();
 
   $('#submit_link').on('click', function() {
     submitText(editor.getValue());
   })
+  $("#reset_storage").click(function(e) {
+    e.preventDefault();
+    delete localStorage["editor"];
+  });
 })
